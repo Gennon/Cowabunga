b8c808a8ba48fc8f998b78546d8421281917ad64
Gennon <imgennon@gmail.com>
2016-01-26 14:54:12 +0100

    Added text to be translated

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import { Button, Jumbotron } from 'react-bootstrap'
+import { Button, Jumbotron } from 'react-bootstrap';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+const messages = defineMessages({
+    greeting: {
+        id: 'app.greeting',
+        description: 'Welcome greeting to the user',
+        defaultMessage: 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.',
+    }
+});
 
 export default class App extends Component {    
     render() {
@@ -7,7 +16,7 @@ export default class App extends Component {
             <div>
                 <Jumbotron>
                     <h1>Cowabunga!</h1>
-                    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <p><FormattedMessage {...messages.greeting} /></p>
                     <p><Button bsStyle="primary">Learn more</Button></p>
                 </Jumbotron>
             </div>
